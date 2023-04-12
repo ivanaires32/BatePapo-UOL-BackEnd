d478e23a67af0c5992e9efc9ea06d96b91cb764b
Ivan <ivanaires32@gmail.com>
2023-04-12 11:34:45 -0300

    adicionar participantes

---
+++ src/app.js
@@ -0,0 +1,41 @@
+import express, { json } from "express"
+import cors from "cors"
+import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
+
+const app = express()
+
+app.use(json())
+app.use(cors())
+dotenv.config()
+
+//config database
+let db
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch(err => console.log(err.message))
+
+app.post("/participants", (req, res) => {
+    const { name } = req.body
+
+    if (!name) {
+        return res.status(422).send("Campo Obrigatorio")
+    } else if (nameUsers.includes(name)) {
+        return res.status(409).send("Usuario já cadastrado")
+    }
+
+    db.collection("participants").insertOne({ name, lastStatus: Date.now() })
+        .then(() => {
+            db.collection("messages").insertOne({ from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: 'HH:mm:ss' })
+        })
+        .catch(() => res.sendStatus(500))
+})
+
+app.get("/participants", (req, res) => {
+    db.collection("participants").find({}).toArray()
+        .then(users => res.send(users))
+        .catch(() => res.sendStatus(500))
+})
+
+app.listen(4000, () => console.log("Servidor rodando"))
